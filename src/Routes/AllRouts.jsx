d666a4b57b45e62215549c5cbb2a00a9ba79e3b9
Pravin Kumar <pravinmimrot648@gmail.com>
2024-10-20 10:20:27 +0530

    Update AllRouts.jsx

--- src/Routes/AllRouts.jsx
+++ src/Routes/AllRouts.jsx
@@ -3,8 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import List from '../pages/playlist/List'
 import PlayListsCont from '../pages/playlist/PlayListsCont'
-import { LoginSuccess } from '../Authentication/loginSuccess'
-// import Login from '../Authentication/Login'
 import { OauthLogin } from '../Authentication/OauthLogin'
 import Login from '../Authentication/Login'
 import { PlayListHome } from '../pages/playlist/PlayListHome'
@@ -15,9 +13,7 @@ export default function AllRouts() {
   return (
     <Routes>
       <Route path={"/"} element={<Home />} />
-      <Route path={"/success"} element={<LoginSuccess />} />
       <Route path='/login' element={<OauthLogin />} />
-      {/* <Route path='/login' element={<Login/>}/> */}
       <Route path={"/product/playlist"} element={<PlayListHome />} />
       <Route path={"/product/playlist/item"} element={<VideoHome/>} />
 
